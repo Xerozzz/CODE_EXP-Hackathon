9b96d732ca0aca57ba78f17945ff5ede1d14f016
ChaiZhiKang <52784361+ChaiZhiKang@users.noreply.github.com>
2020-06-03 15:29:07 +0800

    .
    .

--- Code/www/js/userLogin.js
+++ Code/www/js/userLogin.js
@@ -1,11 +1,13 @@
 function reqListener () {
     var username = document.getElementById("uname");
+    console.log(username);
     var password = document.getElementById("password");
+    console.log(password);
 }
 
 function checkUserCred() {
     var oReq = new XMLHttpRequest();
     oReq.addEventListener("load", reqListener);
-    oReq.open("POST", "localhost:8081/api/user/login");
+    oReq.open("POST", "localhost:8081/api/user/:username");
     oReq.send(username, password);
 }
