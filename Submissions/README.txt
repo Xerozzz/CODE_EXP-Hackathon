f32dafbcf766326510a8191dfae203afa83bdbaa
Xerozz <60227509+Xerozzz@users.noreply.github.com>
2020-06-04 11:34:23 +0800

    Submission Folder

---
+++ Submissions/README.txt
@@ -0,0 +1,29 @@
+This is the README document on how to use OpenHeart. 
+
+Guide:
+
+1. Install the .apk file
+2. Press accept on any warnings that appear which indicate the app is not a Play Protected application
+3. Open the application 
+4. Navigate between the various login/register options and the chat feature.
+
+This application is tested locally on our systems with a MySQL database and JS backend. However, in order for it to work on an android phone, we required
+the use of AWS services like EC2 and DynamoDB. Unfortunatey, we were unable to set it up due to limited time. Hence, this application is just a Proof of Concept.
+
+For the source code, visit: https://github.com/Xerozzz/CODE_EXP-Hackathon
+
+Description of the Application:
+When tasked with the problem statement given to us for this hackathon, we wanted to create an application that is versatile enough to be applied in any period as well as sufficiently impact the lives of people. We wanted to use the application to help as many people as possible. After much research, we realised that during the COVID-19 period, people may get lonely and depressed and need somebody to talk to. Unfortunately, we realised there were not many appplications in Singapore that did this, hence we wanted to create it.
+
+The app provides an easy to use and gentle interface in order to soothe the user. The user can easily pick up their mobile phone and approach a counsellor in less then 3 steps.
+
+Open the app and choose guest login
+Enter a few words or sentences to describe their feelings and emotions
+Wait a few minutes, up to 5, for a counsellor to open a consultation either via call or text chat
+The words input by the user will allow the counsellor to better understand the user and know how to help them best.
+
+The feature of a guest login allows a user to easily access a counsellor without going through tedious steps to register an account. This is gentler and easier on their already fragile and negative emotions.
+
+After a consultation, the guest user has the option to create an account, which will allow us to better cater to him in the future. With a registered account, counsellors can write notes about each user, this will allow them to identify the same user in the future, or have an easier hand off to another counsellor without going back to square one.
+
+Counsellors on this platform will be qualified and from collaborating organizations such as Samaritans of Singapore.
---
+++ Submissions/code_exp.sql
@@ -0,0 +1,110 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: code_exp
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `counsellors`
+--
+
+DROP TABLE IF EXISTS `counsellors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `counsellors` (
+  `counsellorid` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `organization` varchar(255) NOT NULL,
+  PRIMARY KEY (`counsellorid`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `counsellors`
+--
+
+LOCK TABLES `counsellors` WRITE;
+/*!40000 ALTER TABLE `counsellors` DISABLE KEYS */;
+INSERT INTO `counsellors` VALUES (1,'Alfred','Alfred Yes','Alfred','SOS');
+/*!40000 ALTER TABLE `counsellors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `notes`
+--
+
+DROP TABLE IF EXISTS `notes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `notes` (
+  `notesid` int NOT NULL AUTO_INCREMENT,
+  `userid` int NOT NULL,
+  `content` varchar(255) NOT NULL,
+  PRIMARY KEY (`notesid`),
+  UNIQUE KEY `userid_UNIQUE` (`userid`),
+  KEY `userid_idx` (`userid`),
+  CONSTRAINT `userid` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `notes`
+--
+
+LOCK TABLES `notes` WRITE;
+/*!40000 ALTER TABLE `notes` DISABLE KEYS */;
+INSERT INTO `notes` VALUES (1,1,'this guys is amazing!');
+/*!40000 ALTER TABLE `notes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `userid` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`userid`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'jeff','Jeff Chua','jeff');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-06-03 16:31:02
